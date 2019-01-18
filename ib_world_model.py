ff4f9a416b666ff93ff867d97607a6852985f2b8
felixlinker <linkerfelix@gmail.com>
2019-01-18 15:35:43 +0100

    Optimize model generation

--- ib_world_model.py
+++ ib_world_model.py
@@ -3,6 +3,7 @@ from functools import reduce
 from keras import layers, backend
 from keras.models import Model
 from keras.layers import Dense, Input, ZeroPadding1D
+from tensorflow import Tensor
 import numpy
 from argparse import ArgumentParser
 import json
@@ -14,27 +15,33 @@ args = parser.parse_args()
 with open(args.cfg_file) as fp:
     cfg = json.load(fp)
 
-Z_DIM = cfg['z_dim']  # > 1 assumed
-A_DIM = cfg['a_dim']  # > 1 assumed
+Z_DIM = cfg['z_dim']
+assert 0 < Z_DIM
+A_DIM = cfg['a_dim']
+assert 0 < A_DIM
 
-UNROLL_PAST = cfg['unroll_p']  # > 1 assumed
-UNROLL_FUTURE = cfg['unroll_f']  # > 1 assumed
+UNROLL_PAST = cfg['unroll_p']
+assert 1 < UNROLL_PAST
+UNROLL_FUTURE = cfg['unroll_f']
+assert 1 < UNROLL_FUTURE
 
 # note: paper doesn't mention specific activation functions
 s_t_layer = Dense(1, activation='sigmoid')
 si_t_layer= Dense(1, activation='sigmoid')
 y_t_layer = Dense(1, activation='sigmoid')
 
-PLayers = Tuple[list, list, Dense]
-def red_p_layers(i_list: list, o_list: list, o: Dense) -> PLayers:
-    z_t_input = Input(shape=(Z_DIM,))
-    i_list.append(z_t_input)
-    z_t_input = layers.concatenate([o, z_t_input])
-    s_t = s_t_layer(z_t_input)
-    a_t_input = Input(shape=(A_DIM,))
-    i_list.append(a_t_input)
+def build_layer_block(s_t_input: Tensor, z_t_input: Tensor, a_t_input: Tensor) -> Tensor:
+    s_t = s_t_layer(layers.concatenate([s_t_input, z_t_input]))
     si_t = si_t_layer(layers.concatenate([s_t, a_t_input]))
     y_t = y_t_layer(si_t)
+    return (y_t, si_t)
+
+PLayers = Tuple[list, list, Tensor]
+def red_p_layers(i_list: list, o_list: list, o: Tensor) -> PLayers:
+    z_t_input = Input(shape=(Z_DIM,))
+    a_t_input = Input(shape=(A_DIM,))
+    i_list.extend([z_t_input, a_t_input])
+    y_t, si_t = build_layer_block(o, z_t_input, a_t_input)
     o_list.append(y_t)
     return (i_list, o_list, si_t)
 
@@ -45,16 +52,12 @@ initial_input = Input(shape=(1,))
     ([initial_input], [], initial_input)
 )
 
-FLayers = Tuple[list, list, Dense]
-def red_f_layers(i_list: list, o_list: list, o: Dense) -> FLayers:
+FLayers = Tuple[list, list, Tensor]
+def red_f_layers(i_list: list, o_list: list, o: Tensor) -> FLayers:
     z_t_input = Input(shape=(Z_DIM,))
-    i_list.append(z_t_input)
-    z_t_input = layers.concatenate([o, z_t_input])
-    s_t = s_t_layer(z_t_input)
     a_t_input = Input(shape=(3,))
-    i_list.append(a_t_input)
-    si_t = si_t_layer(layers.concatenate([s_t, a_t_input]))
-    y_t = y_t_layer(si_t)
+    i_list.extend([z_t_input, a_t_input])
+    y_t, si_t = build_layer_block(o, z_t_input, a_t_input)
     o_list.append(y_t)
     return (i_list, o_list, si_t)
 
